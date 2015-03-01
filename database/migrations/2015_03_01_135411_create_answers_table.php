bcd3ccf260ee960fd812afc15f14a3c7a09f00c5
Eliraz <fadeys@gmail.com>
2015-03-01 22:55:07 +0200

    init ofek-quiz

---
+++ database/migrations/2015_03_01_135411_create_answers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnswersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('answers', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('question_id')->index();
+			$table->string('name');
+			$table->text('answer');
+			$table->text('explanation');
+			$table->tinyInteger('correct')->default(0);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('answers');
+	}
+
+}
